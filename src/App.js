ed81ef424c4747183b6353772c6ad4c06f3826a7
Doug Johnson <douglas.j.johnson@gmail.com>
2020-03-23 14:03:15 -0600

    base random activity select

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Card from './components/Card'
 
 const BASE_PATH = "http://localhost:3000/activities"
 
 class App extends React.Component {
   state = {
     activities: [],
-    selectedActivity: 0
+    selectedActivity: -1
   }
 
   componentDidMount() {
@@ -15,15 +16,32 @@ class App extends React.Component {
     .then(activities => this.setState({activities}))
   }
 
+  getRandomActivityByType = (type) => {
+    const idsForType = []
+
+    this.state.activities.forEach((activity, index) => {
+      if(activity.activity_type === type) {
+        idsForType.push(index)
+      }
+    })
+
+    const randomCardId = idsForType[Math.floor(Math.random()*idsForType.length)]
+
+    this.setState({selectedActivity: randomCardId})
+  }
+
   render() {
-    console.log(this.state)
+    const selectedActivity = this.state.selectedActivity
+    console.log(selectedActivity)
 
     return (
       <div className="App">
-        <header className="App-header"></header>
-        <body>
-          <Card activity={this.state.activities[this.state.selectedActivity]} />
-        </body>
+        <header className="App-header">
+          <div onClick={() => this.getRandomActivityByType("active")}>Active</div>
+          <div onClick={() => this.getRandomActivityByType("passive")}>Passive</div>
+          <div onClick={() => this.getRandomActivityByType("skill")}>Skill</div>
+        </header>
+        {selectedActivity >= 0 ? <Card activity={this.state.activities[this.state.selectedActivity]} /> : null}
       </div>
     );
   }
